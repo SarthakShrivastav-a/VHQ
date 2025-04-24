622f5f7822c1f3b5b3d044b83a50ba8e1afc6732
Sarthak Shrivastava <161567581+SarthakShrivastav-a@users.noreply.github.com>
2025-04-24 23:08:56 +0530

    Scene Setup and Screen added

---
+++ src/game/PhaserGame.ts
@@ -0,0 +1,28 @@
+import Phaser from 'phaser';
+import OfficeScene from './OfficeScene';
+
+export const gameConfig: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: 'game-container',
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
+    }
+  },
+  scene: [OfficeScene],
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  pixelArt: true
+};
+
+export default class Game extends Phaser.Game {
+  constructor(config: Phaser.Types.Core.GameConfig) {
+    super(config);
+  }
+}
